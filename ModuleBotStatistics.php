71e1955ff2bf3994545f234c4f83c46c2cec491c
Glen Langer <contao.modul@glen-langer.de>
2012-06-08 00:34:17 +0200

    Zwischenstand. Do not use.

--- ModuleBotStatistics.php
+++ ModuleBotStatistics.php
@@ -33,6 +33,9 @@ class ModuleBotStatistics extends Module
 	 */
 	protected $strTemplate = 'mod_botstatistics_fe';
 
+	protected $BotStatus = false;
+	protected $BotName   = '';
+	
 	/**
 	 * Display a wildcard in the back end
 	 * @return string
@@ -58,8 +61,89 @@ class ModuleBotStatistics extends Module
 	 */
 	protected function compile()
 	{
-	    $this->Template->bid = 42;
+	    $this->import('Database');
+	    $this->BotCountUpdate('1'); // Modul ID
+	    return;
 	}
-}
+	
+	/**
+	 * Insert/Update Counter
+	 */
+	protected function BotCountUpdate($bid)
+	{
+	    $ClientIP = bin2hex(sha1($bid . $this->Environment->remoteAddr,true)); // sha1 20 Zeichen, bin2hex 40 zeichen
+	    $BlockTime = 1800; //Sekunden
+	    $CURDATE = date('Y-m-d');
+	    //Bot Blocker
+	    $this->Database->prepare("DELETE FROM tl_botstatistics_blocker"
+                	            ." WHERE CURRENT_TIMESTAMP - INTERVAL ? SECOND > bot_tstamp"
+                	            ." AND bid=? AND bot_ip=?")
+             ->executeUncached($BlockTime, $bid, $ClientIP);
+	    
+	    if ($this->CheckBot() === false) 
+	    {
+	        return false;
+	    }
+	    if ($this->BotName === false) {
+	        //keine Advanced Kennung :-(
+	        $this->BotName = 'noname';
+	    }
+	    // Doppelte Einträge verhindern bei zeitgleichen Zugriffen wenn noch kein Eintrag vorhanden ist
+	    $arrSet = array
+	    (
+	            'bid'          => $bid,
+	            'bot_date'     => $CURDATE,
+	            'bot_name'     => $this->BotName,
+	            'bot_counter'  => 1
+	    );
+	    $this->Database->prepare("INSERT IGNORE INTO tl_botstatistics_counter %s")->set($arrSet)->executeUncached();
+	    
+	    //Bot Visits lesen
+	    $objBotCounter = $this->Database->prepare("SELECT id, bot_counter"
+                                	            ." FROM tl_botstatistics_counter"
+                                	            ." WHERE bid=?"
+                	                            ." AND bot_date=?"
+                                	            ." AND bot_name=?")
+	                          ->executeUncached($bid, $CURDATE, $this->BotName);
+	    $objBotCounter->next();
+	    //zählen per update
+	    $this->Database->prepare("UPDATE tl_botstatistics_counter SET bot_counter=? WHERE id=?")
+	                   ->executeUncached($objBotCounter->bot_counter +1, $objBotCounter->id);
+	    //blocken
+	    $this->Database->prepare("INSERT INTO tl_botstatistics_blocker"
+	                            ." SET bid=?, bot_tstamp=CURRENT_TIMESTAMP, bot_ip=?")
+	                   ->executeUncached($bid, $ClientIP);
+	    return;
+	}
+	
+	/**
+	 * Spider Bot Check
+	 */
+	protected function CheckBot()
+	{
+	    if (!in_array('botdetection', $this->Config->getActiveModules()))
+	    {
+	        //botdetection Modul fehlt, Abbruch
+	        $this->log('BotDetection extension required!', 'ModuleBotStatistics CheckBot', TL_ERROR);
+	        return false;
+	    }
+	    $this->import('ModuleBotDetection');
+	    if ($this->ModuleBotDetection->BD_CheckBotAgent() || $this->ModuleBotDetection->BD_CheckBotIP()) 
+	    {
+	        //log_message('BotStatus True','debug.log');
+	        $this->BotStatus = true;
+	    }
+	    
+	    $this->BotName = $this->ModuleBotDetection->BD_CheckBotAgentAdvanced();
+	    //log_message('BotName: '.$this->BotName,'debug.log');
+	    if ($this->BotStatus === true || $this->BotName !== false) 
+	    {
+	        return true;
+	    }
+	    return false;
+	} //CheckBot
+	
+	
+}//class
 
 ?>
--- config/database.sql
+++ config/database.sql
@@ -16,13 +16,38 @@
 
 CREATE TABLE `tl_botstatistics_counter` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `pid` int(10) unsigned NOT NULL default '0',
-  `sorting` int(10) unsigned NOT NULL default '0',
-  `tstamp` int(10) unsigned NOT NULL default '0',
+  `bid` int(10) unsigned NOT NULL default '0',
+  `bot_date` date NOT NULL default '1999-01-01',
   `bot_name` varchar(60) NOT NULL default 'Unknown',
   `bot_counter` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `pid` (`pid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table `tl_botstatistics_blocker`
+-- 
+
+CREATE TABLE `tl_botstatistics_blocker` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `bid` int(10) unsigned NOT NULL default '0',
+  `bot_tstamp` timestamp NULL default NULL,
+  `bot_ip` varchar(40) NOT NULL default '0.0.0.0',
+  PRIMARY KEY  (`id`),
+  KEY `bid` (`bid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+-- 
+-- Table `tl_module`
+-- 
+
+CREATE TABLE `tl_module` (
+  `botstatistics_name` varchar(64) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;  
 
